89b524afdef6ee7e73c3bcd5e68430428b1381fa
William <William.Mauclet@staff.telenet.be>
2017-02-25 20:31:51 +0100

    Refactorisation ZeroOrderRobot.

--- ZeroOrderRobot.py
+++ ZeroOrderRobot.py
@@ -1,6 +1,9 @@
 import random
 
 class ZeroOrderRobot():
+	"""This robot DOES NOT LEARN. 
+	It applies simple algorithms to see if it can avoid a four in a row.
+	Except for that, it just plays randomly."""
 	def chooseMove(self, grid):
 		freeColumns = grid.getFreeColumns()
 		if checkColumns(grid, freeColumns):
@@ -9,13 +12,14 @@ class ZeroOrderRobot():
 			return random.choice(freeColumns)
 		
 	def checkColumns(self, grid, freeColumns):
-		for column in grid.columns:
-			if hasTopEmptyWithTripletBelow(column):
-				return column
-			elif hasTopEmptyWithAdjacentTriplets(column):
-				return column
-			elif hasTopEmptyWithDiagonalTriplets(column):
-				return column
+		for x in freeColumns:
+			y= findTopEmpty(grid.columns[x])
+			if hasTopEmptyWithTripletBelow(grid.columns[x]):
+				return x
+			elif hasTopEmptyWithAdjacentTriplets(grid, x, y):
+				return x
+			elif hasTopEmptyWithDiagonalTriplets(grid, x, y):
+				return x
 		return False
 	
 	def findTopEmpty(self, column):
@@ -24,24 +28,15 @@ class ZeroOrderRobot():
 			y -= 1
 		return y + 1 
 
-	def hasTopEmptyWithTripletBelow(self, column):
-		y = findTopEmpty(column)
-		
+	def hasTopEmptyWithTripletBelow(self, column, y):
 		if y < 3:
 			return False
-		
-		seq = 0
-		prev = column[y]
-		y -= 1
-		while column[y] is prev:
-			seq +=1
-		if seq is 3:
+		if column[y-1] is column[y-2] is column[y-3] is not None:
 			return True
-		else:	
+		else:
 			return False
 			
-	def hasTopEmptyWithAdjacentTriplets(self, grid, x):
-		y = findTopEmpty(column)
+	def hasTopEmptyWithAdjacentTriplets(self, grid, x, y):
 		if x <= 3 and grid.columns[x+1][y] is grid.columns[x+2][y] is grid.columns[x+3][y] is not None:
 			return True
 		elif x >= 3 and grid.columns[x-1][y] is grid.columns[x-2][y] is grid.columns[x-3][y] is not None:
@@ -49,8 +44,7 @@ class ZeroOrderRobot():
 		else:
 			return False
 	
-	def hasTopEmptyWithDiagonalTriplets(self, grid, x):
-		y = findTopEmpty(grid.columns[x])
+	def hasTopEmptyWithDiagonalTriplets(self, grid, x, y):
 		if y <= 2:
 			if x <= 3 and grid.columns[x+1][y+1] is grid.columns[x+2][y+2] is grid.columns[x+3][y+3] is not None:
 				return True
