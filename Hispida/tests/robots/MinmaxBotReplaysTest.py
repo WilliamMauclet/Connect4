4886b72704a986d93644e395fbfb266263233ad4
WilliamM <william.mauclet@gmail.com>
2017-08-16 22:03:37 +0200

    Hispida:     MinmaxBotReplaysTest & Connect4 both with depth 7 heurs 0 give different results for replay 3.

---
+++ Hispida/tests/robots/MinmaxBotReplaysTest.py
@@ -0,0 +1,56 @@
+import unittest
+# ast.literal_eval converts string representations into actual instances
+#  https://stackoverflow.com/a/1894296
+import ast
+from Grid import Grid
+from MinmaxBot import MinmaxBot
+
+
+class MinmaxBotReplaysTest(unittest.TestCase):
+    def setUp(self):
+        self.file_path = '/home/wmclt/Projects/Connect4/Hispida/docs/replays/minmaxBot_{}.txt'
+        self.minmaxBot_5 = MinmaxBot('B', depth=5)
+        self.minmaxBot_7 = MinmaxBot('B', depth=7, heuristic_opponent=0, heuristic_bot=0)
+        self.grid = Grid()
+
+    def test_5_replay_1(self):
+        with open(self.file_path.format(1), "r") as reader:
+            replay = ast.literal_eval(reader.read())
+            self.grid.add_pawn_history(replay)
+
+            self.assertEqual(self.minmaxBot_5.choose_move(self.grid), 3)
+
+    def test_5_replay_2(self):
+        with open(self.file_path.format(2), "r") as reader:
+            replay = ast.literal_eval(reader.read())
+            self.grid.add_pawn_history(replay)
+
+            self.assertIn(self.minmaxBot_5.choose_move(self.grid), [2, 5])
+
+    def test_5_replay_3(self):
+        with open(self.file_path.format(3), "r") as reader:
+            replay = ast.literal_eval(reader.read())
+            self.grid.add_pawn_history(replay)
+
+            self.assertNotEqual(self.minmaxBot_5.choose_move(self.grid), 1)
+
+    def test_7_replay_1(self):
+        with open(self.file_path.format(1), "r") as reader:
+            replay = ast.literal_eval(reader.read())
+            self.grid.add_pawn_history(replay)
+
+            self.assertEqual(self.minmaxBot_5.choose_move(self.grid), 3)
+
+    def test_7_replay_2(self):
+        with open(self.file_path.format(2), "r") as reader:
+            replay = ast.literal_eval(reader.read())
+            self.grid.add_pawn_history(replay)
+
+            self.assertIn(self.minmaxBot_5.choose_move(self.grid), [2, 5])
+
+    def test_7_replay_3(self):
+        with open(self.file_path.format(3), "r") as reader:
+            replay = ast.literal_eval(reader.read())
+            self.grid.add_pawn_history(replay)
+
+            self.assertNotEqual(self.minmaxBot_5.choose_move(self.grid), 1)
